a3132c5aae1c8a45e2dbe82eae8679917193fb63
David Bayless <David.C.Bayless15@gmail.com>
2016-01-26 13:16:35 -0700

    made stars come towards camera in random generation infinite loop

--- tutorial.js
+++ tutorial.js
@@ -10,24 +10,31 @@ var camera, scene, renderer;
 
         function init() {
 
-            camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-            camera.position.z = 0;
+            camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 800);
+            camera.position.z = -10;
 
             scene = new THREE.Scene();
 
-            var texture = THREE.ImageUtils.loadTexture('nickface.png');
+            // var texture = THREE.ImageUtils.loadTexture('nickface.png');
+            var light = new THREE.DirectionalLight( 0xffff00, 1 );
+            light.position.set(0, 1, 0).normalize();
+            scene.add(light);
 
-            var geometry = new THREE.BoxGeometry(20, 20, 20);
+            var geometry = new THREE.SphereGeometry(1, 1, 16);
             var material = new THREE.MeshBasicMaterial({
-                map: texture
+                // map: texture,
+                ambient: 0xff88aa,
+                specular: 0x555555,
+                shininess: 50,
+                color: '#efefef'
             });
 
             var radius = 1600;
-            var nBoxes = 10000;
+            var nBoxes = 20000;
             for (var i = 0; i <nBoxes; i++) {
                 mesh = new THREE.Mesh(geometry, material);
                 scene.add(mesh);
-                mesh.position.set(radius/2 -radius * Math.random(), radius/2 -radius * Math.random(), radius/2 -radius * Math.random() - 100);
+                mesh.position.set(radius/2 -radius * Math.random(), radius/2 -radius * Math.random(), radius/2 -radius * Math.random());
                 enimies.push(mesh);
             }
 
@@ -56,10 +63,14 @@ var camera, scene, renderer;
             requestAnimationFrame(animate);
 
             for(var i = 0; i < enimies.length; i++) {
-                if(enimies[i].position.y < -800) {
-                    enimies[i].position.y = 800;
+                if(enimies[i].position.z > 0) {
+                    enimies[i].position.z = Math.random() * -800;
+                    enimies[i].position.y = Math.random() * -800 + Math.random() * 800;
+                    enimies[i].position.x = Math.random() * -800 + Math.random() * 800;
+
                 } else {
-                    enimies[i].position.y -= enemySpeed;
+                    enimies[i].position.z += enemySpeed;
+
                 }
             }
 
